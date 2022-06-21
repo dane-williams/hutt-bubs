996a493b1d9404d4221f0296edfc5685577adf3b
dane-williams <uhtiger@gmail.com>
2022-06-21 20:42:49 +1200

    continue icons

--- client/components/Parks.jsx
+++ client/components/Parks.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaRestroom, FaChild, FaBiking, FaParking, FaBabyCarriage, FaSkating } from 'react-icons/fa'
+import { FaRestroom, FaChild, FaBiking, FaParking, FaBabyCarriage, FaSkating, FaHiking } from 'react-icons/fa'
 
 function Parks () {
   return (
@@ -10,7 +10,7 @@ function Parks () {
       <div className='parks-page'>
         <div className='print_before'>
           <div>
-            <h2>Avalon</h2><FaRestroom />&nbsp;<FaChild />&nbsp;<FaBiking />&nbsp;<FaParking />&nbsp;<FaBabyCarriage />&nbsp;<FaSkating />
+            <h2><a href='https://goo.gl/maps/ngNknbUMHCQkSGgT9'>Avalon</a></h2><div className='icons'><FaRestroom />&nbsp;<FaChild />&nbsp;<FaBiking />&nbsp;<FaParking />&nbsp;<FaBabyCarriage />&nbsp;<FaSkating /></div>
             <p>Possibly the best park in the Hutt Valley. Upgraded recently. Has a very good play area for the slightly older kids. A water play area and a small play area for the younger ones. All this plus a flat bike track, skate park, duck pond, seasonal train rides and brand new toilet block make it a must for parents looking to keep children entertained. </p>
           </div>
           <div className='pic-div'><img className='parks' src='/images/Avalon Park.jpeg'/></div>
@@ -18,7 +18,7 @@ function Parks () {
 
         <div className='print_after'>
           <div>
-            <h2>Parkay Play Park</h2><div className='icons'><FaRestroom />&nbsp;<FaChild />&nbsp;<FaParking />&nbsp;<FaBabyCarriage /></div>
+            <h2><a href='https://goo.gl/maps/Q8ZWxbUWiXhN1UpH8'>Parkay Family Playground</a></h2><div className='icons'><FaRestroom />&nbsp;<FaChild />&nbsp;<FaParking />&nbsp;<FaBabyCarriage /></div>
             <p>Has two main areas. An older area that has flying fox and obstacle style park. Much better for the older kids. Its biggest plus is the newly built play area for the smaller kids. Decent play area but fully fenced. The park has recently had a new toilet built and is only a few hundred meters from a brand new dog park.</p>
           </div>
           <div className='pic-div'><img className='parks' src='/images/parkway.jpeg'/></div>
@@ -26,7 +26,7 @@ function Parks () {
 
         <div className='print_before'>
           <div>
-            <h2>Maidstone Park</h2><FaRestroom />&nbsp;<FaChild />&nbsp;<FaBiking />&nbsp;<FaParking />&nbsp;<FaBabyCarriage />&nbsp;<FaSkating />
+            <h2><a href='https://goo.gl/maps/KVRMv1LS9vN7KskZ7'>Maidstone Park</a></h2><div className='icons'><FaRestroom />&nbsp;<FaChild />&nbsp;<FaBiking />&nbsp;<FaParking />&nbsp;<FaBabyCarriage />&nbsp;<FaSkating /></div>
             <p>The newest of the Hutt Valley parks. Another excellent area filled with plenty to do. Castles, musical instruments, flying fox, skate park and brand new toilet area. Close to a local ice skating rink and Brewtown which has go-carts and paint ball.  </p>
           </div>
           <div className='pic-div'><img className='parks' src='/images/maidstone.jpeg'/></div>
@@ -34,7 +34,7 @@ function Parks () {
 
         <div className='print_after'>
           <div>
-            <h2>Percy Scenic Reserve</h2>
+            <h2>Percy Scenic Reserve</h2><div className='icons'><FaRestroom />&nbsp;<FaParking />&nbsp;<FaHiking /></div>
             <p>Percy Scenic Reserve is a beautifully unique park just on the edge of Lower Hutt City. With a mix of garden area, easy trails, a small cave and a waterfall it’s a perfect spot for youngsters to have their first forest or trail experience amongst some rare native plants.</p>
           </div>
           <div className='pic-div'><img className='parks' src='/images/percy.png'/></div>
@@ -53,7 +53,7 @@ function Parks () {
             <h2>Hugh Sinclair Park</h2>
             <p>There is a skate park behind the playground for older children. Plenty of parking is available on the Strand.</p>
           </div>
-          <div className='pic-div'><img className='parks' src='/images/High Sinclair Park Wainui.jpeg'/>wehhweerhweerhwerhwher</div>
+          <div className='pic-div'><img className='parks' src='/images/High Sinclair Park Wainui.jpeg'/></div>
         </div>
 
         <div className='print_before'>
